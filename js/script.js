4173b921b002948e5c5ea671da35ab0e6ab2cb2f
MattiasKallio <kallio@snillrik.se>
2014-07-16 20:52:29 +0200

    sdasd

--- js/script.js
+++ js/script.js
@@ -874,9 +874,9 @@ function onNotificationGCM(e) {
 		case 'message':
 	    	// this is the actual push notification. its format depends
 			// on the data model from the push server
-	        alert('message = '+e.message + 'message count = '+e.msgcnt + 'battle_id = '+e.battle_id);
+	        alert('message = ' + e.message + 'message count = ' + e.msgcnt + 'battle_id = ' + e.payload.battle_id);
 			var page = "battle.php";
-			fetchInfo(e.battle_id, page);
+			fetchInfo(e.payload.battle_id, page);
 			
 		break;
 	 
