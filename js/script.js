9c65eb0b7b47a608d609cbf1e0f2b5e24e82b8f3
MattiasKallio <kallio@snillrik.se>
2014-07-16 18:13:18 +0200

    testtest

--- js/script.js
+++ js/script.js
@@ -860,7 +860,7 @@ function pushErrorHandler(error) {
     alert("push error: "+error);
 }
 function onNotificationGCM(e) {
-	alert(e.event);
+	//alert(e.event);
 	 switch( e.event )
 	 {
 		case 'registered':
@@ -874,7 +874,9 @@ function onNotificationGCM(e) {
 		case 'message':
 	    	// this is the actual push notification. its format depends
 			// on the data model from the push server
-	        alert('message = '+e.message);
+	        alert('message = '+e.message + 'message count = '+e.msgcnt);
+			var page = "battle.php";
+			fetchInfo(e.battle_id, page);
 			
 		break;
 	 
